11b24815be0885c242162d05282e230e42d6fd0c
Panzerkampfwagen-del <aryanxp.2006@gmail.com>
2025-05-21 08:41:08 +0000

    Update solution7-2

--- Solutions/Part-7/solution7-2.py
+++ Solutions/Part-7/solution7-2.py
@@ -24,68 +24,89 @@ The notation [condition] refers to any expression in the format [value] [compari
 
 #own_language
 def run(instructions: list) -> list:
-    stack = [0] * 26
     pre_variables = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result = []
-    track = {}
+    result=[]
+    track={}
+    stack=[0]*26
 
-    for idx, line in enumerate(instructions):
-        if ":" in line:
-            parts = line.split(":")
-            label = parts[0]
-            track[label] = idx
-            instructions[idx] = ""
+    for i,instruction in enumerate(instructions):
+        if ":" in instruction:
+            pos=instruction.split(":")
+            label=pos[0]
+            track[label]=i
+            instructions[i]=""
 
-    i = 0
-    while i < len(instructions):
-        line = instructions[i]
-        if line == "END":
-            break
-        if not line:
-            i += 1
+    i=0
+    while i<len(instructions):
+        if instructions[i]=="END":
+            return result
+        
+        if instructions[i]=="":
+            i+=1
+            continue
+        
+        instruction=instructions[i].split()
+        ops=instruction[0]
+        
+        if ops=="ADD":
+            pos=pre_variables.index(instruction[1])
+            if instruction[2] in pre_variables:
+                val=pre_variables.index(instruction[2])
+                stack[pos]+=stack[val]
+            else:
+                val=int(instruction[2])
+                stack[pos]+=val
+
+        elif ops=="SUB":
+            pos=pre_variables.index(instruction[1])
+            if instruction[2] in pre_variables:
+                val=pre_variables.index(instruction[2])
+                stack[pos]-=stack[val]
+            else:
+                val=int(instruction[2])
+                stack[pos]-=val
+            
+
+        elif ops=="MUL":
+            pos=pre_variables.index(instruction[1])
+            if instruction[2] in pre_variables:
+                val=pre_variables.index(instruction[2])
+                stack[pos]*=stack[val]
+            else:
+                val=int(instruction[2])
+                stack[pos]*=val
+
+        elif ops=="MOV":
+            pos=pre_variables.index(instruction[1])
+            if instruction[2] in pre_variables:
+                val=pre_variables.index(instruction[2])
+                stack[pos]=stack[val]
+            else:
+                val=int(instruction[2])
+                stack[pos]=val
+
+
+        elif ops=="PRINT":
+            if instruction[1] in pre_variables:
+                val=pre_variables.index(instruction[1])
+                result.append(stack[val])
+            else:
+                val=int(instruction[1])
+                result.append(val)
+
+        elif ops=="JUMP":
+            label=instruction[1]
+            i=track[label]
             continue
 
-        instruction = line.split()
-        op = instruction[0]
-
-        def get_value(val):
-            return stack[pre_variables.index(val)] if val in pre_variables else int(val)
-
-        if op == "MOV":
-            pos = pre_variables.index(instruction[1])
-            stack[pos] = get_value(instruction[2])
-
-        elif op == "ADD":
-            p1 = pre_variables.index(instruction[1])
-            stack[p1] += get_value(instruction[2])
-
-        elif op == "SUB":
-            p1 = pre_variables.index(instruction[1])
-            stack[p1] -= get_value(instruction[2])
-
-        elif op == "MUL":
-            p1 = pre_variables.index(instruction[1])
-            stack[p1] *= get_value(instruction[2])
-
-        elif op == "JUMP":
-            label = instruction[1]
-            i = track[label]
-            continue
-
-        elif op == "IF":
-            jump_index = instruction.index("JUMP")
-            condition = " ".join(instruction[1:jump_index])
-            label = instruction[jump_index + 1]
-            env = {ch: stack[pre_variables.index(ch)] for ch in pre_variables}
-            if eval(condition, env):
-                i = track[label]
+        elif ops=="IF":
+            condition="".join([instruction[1],instruction[2],instruction[3]])
+            env={ch:stack[pre_variables.index(ch)] for ch in pre_variables}
+            if eval(condition,{},env):
+                label=instruction[5]
+                i=track[label]
                 continue
-
-        elif op == "PRINT":
-            val = instruction[1]
-            result.append(get_value(val))
-
-        i += 1
+        i+=1
 
     return result
 
